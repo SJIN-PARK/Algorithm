90830478d62a830ac49442734dca550a2259365c
SJIN-PARK <keither0627@gmail.com>
2022-02-26 16:57:14 -0800

    LinkedList

--- Swap_Pairs.java
+++ Swap_Pairs.java
@@ -1,24 +1,55 @@
 package LeetCode;
 
+/*
+Given a linked list, swap every two adjacent nodes and return its head. 
+You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+Input: head = []
+Output: []
+
+Example 3:
+Input: head = [1]
+Output: [1]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
 public class Swap_Pairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-			
+		swapPairs();
 			
 	}
 	
 	
 	public static ListNode swapPairs(ListNode head) {
-		if(head==null) return null;
-		if(head.next==null) return head;
-		ListNode temp=new ListNode();
-		temp.val=head.next.val;
-		head.next.val=head.val;
-		head.val=temp.val;
-		swapPairs(head.next.next);
-		return head;
+		
+		if(head == null){ return head;}
+        if(head.next == null){ return head;}
+        
+        ListNode temp_list = new ListNode();        
+        temp_list.val = head.next.val;
+        head.next.val = head.val;
+        head.val = temp_list.val;
+        swapPairs(head.next.next);
+        
+        return head;
 
 	}
 
