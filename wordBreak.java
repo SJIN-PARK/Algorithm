6c3a0696b4f697da09f43631b287792e393f69c9
SJIN-PARK <keither0627@gmail.com>
2022-03-16 23:15:44 -0700

    Update wordBreak.java

--- wordBreak.java
+++ wordBreak.java
@@ -10,12 +10,27 @@ public class wordBreak {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "catsandog";
-		List<String> wordDict = new ArrayList<>();
+		String[] wordDict = {"cats","dog","sand","and","cat"};
+		
+		String[] res = new String[s.length()+1];
 		
 		String temp_s = s;
 		Set<String> set = new HashSet<>();
-		for(int i = 0; i < wordDict.size(); i++){
-			set.add(wordDict.get(i));
+		for(int i = 0; i < wordDict.length; i++){
+			set.add(wordDict[i]);
+		}
+		
+		for(int i = 0; i < s.length(); i++){
+			for(int j = i+1; j < s.length(); j++){
+				if(res[j] != null){
+					continue;
+				}
+				
+				if(set.contains(temp_s.subSequence(i, j))){
+					System.out.println(temp_s.subSequence(i, j));
+					res[j] = temp_s.substring(i, j);
+				}
+			}
 		}
 	}
 
