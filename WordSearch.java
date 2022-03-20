a604ad1ebe9a688daccf103757cf124b9247965b
SJIN-PARK <keither0627@gmail.com>
2022-03-19 19:28:16 -0700

    BackTracking

--- WordSearch.java
+++ WordSearch.java
@@ -13,17 +13,17 @@ where adjacent cells are horizontally or vertically neighboring. The same letter
 
 Example 1:
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Input: board = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], word = 'ABCCED'
 Output: true
 
 Example 2:
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Input: board = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], word = 'SEE'
 Output: true
 
 Example 3:
 
-Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Input: board = [['A','B','C','E'],['S','F','C','S'],['A','D','E','E']], word = 'ABCB'
 Output: false
 
 Constraints:
@@ -39,25 +39,63 @@ public class WordSearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[][] board = {{"A","B","C","E"},{"S","F","C","S"},{"A","D","E","E"}};
-		String word = "ABCCED";		
+		char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+		String word = "ABCB";		
 		int n = board.length; //row
 		int m = board[0].length; // column
-		
-		HashMap<String, Integer> wordMap = new HashMap<>();
-		for(int i = 0; i < word.length(); i++){
-			String curStr = Character.toString(word.charAt(i));
-			if(!wordMap.containsKey(curStr)){
-				wordMap.put(curStr, 1);
-			}else{
-				int count = wordMap.get(curStr);
-				wordMap.put(curStr, count + 1);
+		boolean result = false;
+				
+		for(int row = 0; row < board.length; row++){
+			for(int col = 0; col < board[0].length; col++){
+				
+				if(board[row][col] == word.charAt(0)){
+					result = findNextLetter(row, col, 0, board, word);
+					if(result){
+						System.out.println(result);
+					}
+
+				}
 			}
 		}
 		
+		System.out.println(result);
+	}
+	
+	public static boolean findNextLetter(int row, int col, int letterIndex, char[][] board, String word){
+		
+		boolean flag = false;
+			
+		if(row < 0 || col < 0){
+			return false;
+		}
+		if(row >= board.length || col >= board[0].length){
+			return false;
+		}
+		
+		if(letterIndex >= word.length()){
+			return true;
+		}
+			
+		if(board[row][col] == word.charAt(letterIndex)){
+			//System.out.println(board[row][col]);		
+			//System.out.println(resStr);		
+			
+			// check your surroundings
+	        char temp = board[row][col];
+	        // tell the board you have visited this position
+	        board[row][col] = '.';
+			
+			flag =  findNextLetter(row - 1, col, letterIndex+1, board, word)||
+					findNextLetter(row + 1, col, letterIndex+1, board, word)||
+					findNextLetter(row, col - 1, letterIndex+1, board, word)||
+					findNextLetter(row, col + 1, letterIndex+1, board, word);
+			
+			board[row][col] = temp;
+		}
 		
 		
-
+		return flag;
+		
 	}
 
 }
--- wordBreak.java
+++ wordBreak.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,26 +13,53 @@ public class wordBreak {
 		String s = "catsandog";
 		String[] wordDict = {"cats","dog","sand","and","cat"};
 		
-		String[] res = new String[s.length()+1];
+//		String s = "leetcode"; 
+//		String[] wordDict = {"leet","code"};
 		
-		String temp_s = s;
+		Boolean [] visit = new Boolean [s.length()];
+		boolean result = false;
+		
 		Set<String> set = new HashSet<>();
 		for(int i = 0; i < wordDict.length; i++){
 			set.add(wordDict[i]);
 		}
 		
-		for(int i = 0; i < s.length(); i++){
-			for(int j = i+1; j < s.length(); j++){
-				if(res[j] != null){
-					continue;
-				}
-				
-				if(set.contains(temp_s.subSequence(i, j))){
-					System.out.println(temp_s.subSequence(i, j));
-					res[j] = temp_s.substring(i, j);
-				}
-			}
-		}
+//		for(int i = 0; i < s.length(); i++){
+//			for(int j = i+1; j < s.length(); j++){
+//				if(res[j] != null){
+//					continue;
+//				}
+//				
+//				if(set.contains(temp_s.subSequence(i, j))){
+//					System.out.println(temp_s.subSequence(i, j));
+//					res[j] = temp_s.substring(i, j);
+//				}
+//			}
+//		}
+		
+		result = solve(0, s, set, visit, s.length() );
+		System.out.println(Arrays.toString(visit));
+		System.out.println(result);
 	}
+	
+	public static boolean solve(int i, String s, Set<String> wordSet, Boolean [] visit, int l){
+        
+        if(i==l) return true;
+        if(visit[i] != null) return visit[i];
+        boolean flag = false;
+        for(int k=i+1; k<=l; k++){
+            String ss = s.substring(i, k);
+            System.out.println(ss);
+            if(wordSet.contains(ss)){
+            	System.out.println(ss);
+                flag = flag || solve(k, s, wordSet, visit, l);
+                if(flag){
+                	visit[i] = true;
+                    return true;
+                }
+            }
+        }
+        return visit[i] = flag;
+    }
 
 }
